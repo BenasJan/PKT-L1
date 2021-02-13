fe803ef8d75197b5d691d46422b863f6dda25c25
Benas <benas.janusonis@outlook.com>
2021-02-13 21:38:13 +0200

    refactor

--- utils.py
+++ utils.py
@@ -1,34 +1,33 @@
-from friend import Friend
-
-def getFriendsCountInFriendNetwork(network: [], alreadyAddedFriendNames: list = [], shouldReset = True) -> int:
+def getFriendsCountInFriendNetwork(network: [], alreadyAddedFriendNames: list = [], shouldReset=True) -> int:
     if shouldReset:
         alreadyAddedFriendNames = []
 
-    allSubfriendsFromNetWork: list = []
+    allSubFriendsFromNetWork: list = []
     for friend in network:
-        allSubfriendsFromNetWork += friend.friends
+        allSubFriendsFromNetWork += friend.friends
 
     alreadyAddedFriendNames += map(lambda fr: fr.name, network)
-    alreadyAddedFriendNames += map(lambda fr: fr.name, allSubfriendsFromNetWork)
+    alreadyAddedFriendNames += map(lambda fr: fr.name, allSubFriendsFromNetWork)
     alreadyAddedFriendNames = list(set([name for name in alreadyAddedFriendNames]))
 
-    allSubfriendsDeeper: list = []
-    for friend in allSubfriendsFromNetWork:
-        allSubfriendsDeeper += friend.friends
+    allSubFriendsDeeper: list = []
+    for friend in allSubFriendsFromNetWork:
+        allSubFriendsDeeper += friend.friends
 
-    for aafn in alreadyAddedFriendNames:
-        allSubfriendsDeeper = list(filter(lambda fr : fr.name != aafn, allSubfriendsDeeper))
+    for alreadyAddedFriendName in alreadyAddedFriendNames:
+        allSubFriendsDeeper = list(filter(lambda fr: fr.name != alreadyAddedFriendName, allSubFriendsDeeper))
 
     count = len(list(alreadyAddedFriendNames))
 
-    if len(allSubfriendsDeeper) > 0:
-        return getFriendsCountInFriendNetwork(allSubfriendsFromNetWork, alreadyAddedFriendNames, False)
+    if len(allSubFriendsDeeper) > 0:
+        return getFriendsCountInFriendNetwork(allSubFriendsFromNetWork, alreadyAddedFriendNames, False)
     else:
         return count
 
+
 def printAllFriends(friends: []):
     for friend in friends:
-        print('%s has these friends:' %(friend.name))
+        print('%s has these friends:' % friend.name)
         friendsOfFriend = friend.friends
         for friendOfFriend in friendsOfFriend:
-            print('%s' %(friendOfFriend.name))
+            print('%s' % friendOfFriend.name)
